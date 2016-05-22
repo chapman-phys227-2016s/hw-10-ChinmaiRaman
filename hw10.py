5db6436ac2bf5ee63561dab141ba207b04f94ae0
Chinmai Raman <raman105@mail.chapman.edu>
2016-05-22 19:40:02 +0000

    first commit

---
+++ hw10.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+"""
+File: final.py
+Copyright (c) 2016 Chinmai Raman
+License: MIT
+Course: PHYS227
+Assignment: Homework 10
+Date: May 22, 2016
+Email: raman105@mail.chapman.edu
+Name: Chinmai Raman
+Description: Solving ODEs using various methods
+"""
+
+
